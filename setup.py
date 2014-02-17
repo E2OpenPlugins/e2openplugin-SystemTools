2fe60c54555f66f607aa7fe96054d95daf144e33
christophecvr <stefansat@telenet.be>
2014-02-17 16:52:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 pkg = 'Extensions.SystemTools'
 setup (name = 'enigma2-plugin-extensions-systemtools',
-       version = '0.2',
+       version = '0.3',
        description = 'SystemTools for enigma2 stb',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
