13f4eb7274f42fd97c55ec60917d3dc58de0e796
christophecvr <stefansat@telenet.be>
2014-02-18 19:09:07 +0100

    Improving simple memory view
    	modified:   plugin/memorysimple.sh
    	modified:   setup.py

--- plugin/memorysimple.sh
+++ plugin/memorysimple.sh
@@ -6,25 +6,25 @@ cached=`cat /proc/meminfo | grep -m 1 "Cached:" | sed -e 's/Cached://g' | sed -e
 swapcached=`cat /proc/meminfo | grep "SwapCached:" | sed -e 's/SwapCached://g' | sed -e 's/\ kB//g' | sed -e 's/^[ \t]*//'`
 swaptotal=`cat /proc/meminfo | grep "SwapTotal:" | sed -e 's/SwapTotal://g' | sed -e 's/\ kB//g' | sed -e 's/^[ \t]*//'`
 swapfree=`cat /proc/meminfo | grep "SwapFree" | sed -e 's/SwapFree://g' | sed -e 's/\ kB//g' | sed -e 's/^[ \t]*//'`
+meminuse=$(($memtotal - $memfree))
 memused=$(($memtotal - $memfree - $cached - $buffers))
 swapused=$(($swaptotal - $swapfree))
 avblmemory=$(($memfree + $buffers + $cached))
 avblmemorywswap=$(($memfree + $buffers + $cached + $swapfree))
-echo "Total memory:	$memtotal kB"
-echo "Used memory:	$memused kB note: this is just the unusable mem"
-echo "Free memory:	$memfree kB note: this just the unused mem"
-echo "Buffers:    	$buffers kB"
-echo "Cached :    	$cached kB"
+echo "	Total	Used	Free"
 echo ""
-echo "Actual base memory still avbl to apllications: $avblmemory kb"
-#echo "SwapCached :		$swapcached kB"
+echo "mem: 	$memtotal kB	$meminuse kB	$memfree kB"
+echo "-/+BufCaches		$memused kB	$avblmemory kB"
 if [ "$swaptotal" -gt 0 ] ; then
+echo "swap:	$swaptotal kB	$swapused kB	$swapfree kB"
 echo ""
-echo "SwapTotal:  	$swaptotal kB"
-echo "Swapused:	$swapused kB"
-echo "SwapFree:  	$swapfree kB"
+echo "The avbl memory for applications = $avblmemorywswap kB"
+echo "Why? please google linux help DO NOT !! ask at PLI team"
+else
 echo ""
-echo "Total memory(with swap) still avbl to applications: $avblmemorywswap kB"
+echo "The avbl memory for applications = $avblmemory kB"
+echo "Why? please google linux help DO NOT !! ask at PLI team"
 fi
+echo ""
 
 #end
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 pkg = 'Extensions.SystemTools'
 setup (name = 'enigma2-plugin-extensions-systemtools',
-       version = '0.3',
+       version = '0.4',
        description = 'SystemTools for enigma2 stb',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
