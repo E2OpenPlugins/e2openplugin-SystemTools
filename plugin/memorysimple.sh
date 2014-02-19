e9a0b4aa216f8983395ea4f6fcf4e24f17ae3daa
christophecvr <stefansat@telenet.be>
2014-02-19 22:36:43 +0100

    small comment adaptation
    	modified:   plugin/memorysimple.sh

--- plugin/memorysimple.sh
+++ plugin/memorysimple.sh
@@ -19,11 +19,11 @@ if [ "$swaptotal" -gt 0 ] ; then
 echo "swap:	$swaptotal kB	$swapused kB	$swapfree kB"
 echo ""
 echo "The avbl memory for applications = $avblmemorywswap kB"
-echo "Why? please google linux help DO NOT !! ask at PLI team"
+echo "Why? please google linux help "
 else
 echo ""
 echo "The avbl memory for applications = $avblmemory kB"
-echo "Why? please google linux help DO NOT !! ask at PLI team"
+echo "Why? please google linux help "
 fi
 echo ""
 
